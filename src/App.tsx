1a89f73b0f57ec4a9f456a0eb234995f7c833e1b
RenzoLaporno <renzogabriel-19@cpu.edu.ph>
2025-01-15 06:02:31 +0800

    fix:pity issue for 1 wihs

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { fetchAllCharacters } from "./data/fetchCharacter";
 import { useEffect, useState } from "react";
 import { Character, Weapon } from "./types/genshinTypes";
 import { fetchAllWeapon } from "./data/fetchWeapon";
-
+import background from "./assets/background.jpg"
 const App: React.FC = () => {
   const [characters, setCharacters] = useState<Character[]>([]);
   const [weapons, setWeapons] = useState<Weapon[]>([]);
@@ -14,7 +14,7 @@ const App: React.FC = () => {
   const [last5StarCharacter, setLast5StarCharacter] = useState<boolean>(false);
   const [last5StarWeapon, setLast5StarWeapon] = useState<boolean>(false);
   const [pulledItems, setPulledItems] = useState<(Character | Weapon)[]>([]);
-
+  const [primo, setprimo] = useState<number>(3600);
   useEffect(() => {
     (async () => {
       try {
@@ -42,62 +42,45 @@ const App: React.FC = () => {
 
     if (wishType === "character") {
       if (pityCounterCharacter === 90 || last5StarCharacter) {
-        // Guaranteed 5-star on hard pity or after last 5-star
         const fiveStarCharacters = characters.filter((x) => x.rarity === 5);
-        pulledItem =
-          fiveStarCharacters[
-            Math.floor(Math.random() * fiveStarCharacters.length)
-          ];
-        setLast5StarCharacter(false); // Reset last 5-star tracker
-        setPityCounterCharacter(0); // Reset pity counter
-      } else if (rand >= 95) {
-        // 5-star character (5% chance)
+        pulledItem = fiveStarCharacters[Math.floor(Math.random() * fiveStarCharacters.length)];
+        setLast5StarCharacter(false);
+        setPityCounterCharacter(0);
+      } else if (rand <= 0.06) {
         const fiveStarCharacters = characters.filter((x) => x.rarity === 5);
-        pulledItem =
-          fiveStarCharacters[
-            Math.floor(Math.random() * fiveStarCharacters.length)
-          ];
+        pulledItem = fiveStarCharacters[Math.floor(Math.random() * fiveStarCharacters.length)];
         setLast5StarCharacter(true);
         setPityCounterCharacter(0);
-      } else if (rand >= 80) {
+      } else if (rand <= 0.5|| pityCounterCharacter%10 == 0) {
         // 4-star character (15% chance)
         const fourStarCharacters = characters.filter((x) => x.rarity === 4);
-        pulledItem =
-          fourStarCharacters[
-            Math.floor(Math.random() * fourStarCharacters.length)
-          ];
+        pulledItem = fourStarCharacters[Math.floor(Math.random() * fourStarCharacters.length)];
       } else {
-        // 1-3 star item (80% chance)
         const lowerRarityItems = weapons.filter(
           (x) => x.rarity >= 1 && x.rarity <= 3
         );
-        pulledItem =
-          lowerRarityItems[Math.floor(Math.random() * lowerRarityItems.length)];
+        pulledItem = lowerRarityItems[Math.floor(Math.random() * lowerRarityItems.length)];
       }
 
       setPityCounterCharacter((prev) => prev + 1);
     } else if (wishType === "weapon") {
       if (pityCounterWeapon === 80 || last5StarWeapon) {
-        // Guaranteed 5-star on hard pity or after last 5-star
         const fiveStarWeapons = weapons.filter((x) => x.rarity === 5);
         pulledItem =
           fiveStarWeapons[Math.floor(Math.random() * fiveStarWeapons.length)];
-        setLast5StarWeapon(false); // Reset last 5-star tracker
-        setPityCounterWeapon(0); // Reset pity counter
+        setLast5StarWeapon(false);
+        setPityCounterWeapon(0);
       } else if (rand >= 95) {
-        // 5-star weapon (5% chance)
         const fiveStarWeapons = weapons.filter((x) => x.rarity === 5);
         pulledItem =
           fiveStarWeapons[Math.floor(Math.random() * fiveStarWeapons.length)];
         setLast5StarWeapon(true);
         setPityCounterWeapon(0);
       } else if (rand >= 80) {
-        // 4-star weapon (15% chance)
         const fourStarWeapons = weapons.filter((x) => x.rarity === 4);
         pulledItem =
           fourStarWeapons[Math.floor(Math.random() * fourStarWeapons.length)];
       } else {
-        // 1-3 star weapon (80% chance)
         const lowerRarityItems = weapons.filter(
           (x) => x.rarity >= 1 && x.rarity <= 3
         );
@@ -191,40 +174,66 @@ const App: React.FC = () => {
 
     setPulledItems((prev) => [...newPulledItems, ...prev]);
   };
+  const handleClick = (): void => {
+    if (primo <= 160) {
+      console.log("Not enough");
+    } else {
+      setprimo(primo - 160);  
+      performWish();  
+    }
+  };
+  const handleClick10 = (): void => {
+    if (primo <= 1600) {
+      console.log("Not enough");
+    } else {
+      setprimo(primo - 1600);  
+      handleTenWishes();  
+    }
+  };
 
   if (loading) {
     return <div>Loading...</div>;
   }
 
   return (
-    <div>
-      <h1>Genshin Impact Wish System</h1>
+      <div
+        className="min-h-screen min-w-screen bg-cover"
+        style={{
+          backgroundImage: `url(${background})`,
+        }}
+      >
       <div>
-        <button onClick={() => setWishType("character")}>
-          Wish for Character
-        </button>
-        <button onClick={() => setWishType("weapon")}>Wish for Weapon</button>
-      </div>
-      <div>
-        <h2>
-          {wishType === "character" ? "Character" : "Weapon"} Pity Counter:{" "}
-          {wishType === "character" ? pityCounterCharacter : pityCounterWeapon}
-        </h2>
+        <h1>Genshin Impact Wish System</h1>
+        <h1>primo {primo}</h1>
         <div>
-          <button onClick={() => performWish()}>Pull 1 Wish</button>
-          <button onClick={() => handleTenWishes()}>Pull 10 Wishes</button>
+          <button onClick={() => setWishType("character")}>
+            Wish for Character
+          </button>
+          <button onClick={() => setWishType("weapon")}>Wish for Weapon</button>
         </div>
-      </div>
-
-      <h2>Wished Items:</h2>
-      <ul>
-        {pulledItems.map((item, index) => (
-          <div key={index}>
-            <div>{item.name}</div>
-            <div>Rarity: {item.rarity}</div>
+        <div>
+          <h2>
+            {wishType === "character" ? "Character" : "Weapon"} Pity Counter:{" "}
+            {wishType === "character"
+              ? pityCounterCharacter
+              : pityCounterWeapon}
+          </h2>
+          <div>
+            <button onClick={()=>handleClick()}>Pull 1 Wish</button>
+            <button onClick={() => handleClick10()}>Pull 10 Wishes</button>
           </div>
-        ))}
-      </ul>
+        </div>
+
+        <h2>Wished Items:</h2>
+        <ul>
+          {pulledItems.map((item, index) => (
+            <div key={index}>
+              <div>{item.name}</div>
+              <div>Rarity: {item.rarity}</div>
+            </div>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
